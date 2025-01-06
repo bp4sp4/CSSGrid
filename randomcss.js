e49efe94d10c691d187b05b3f850a7fcde15fc2b
Sang Hun Park <62207757+bp4sp4@users.noreply.github.com>
2025-01-06 16:01:29 +0900

    - 미니 프로젝트 완성 - 버튼 올리면 다운로드 버튼 - 스크롤 내리면 위로 올라오는 형식으로 눈을 심심하게 안함

--- randomcss.js
+++ randomcss.js
@@ -2,8 +2,34 @@ document.addEventListener("DOMContentLoaded", function () {
   const items = document.querySelectorAll(".grid__wrapper > div");
   const classes = ["wide", "big", "tall"];
 
+  // 랜덤 클래스 부여
+
   items.forEach((item) => {
     const randomClass = classes[Math.floor(Math.random() * classes.length)];
-    item.className = randomClass;
+    item.classList.add(randomClass);
   });
+
+  // 이미지 위치 확인
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("in-view");
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+    }
+  );
+
+  items.forEach((item) => observer.observe(item));
 });
+
+// 파일 다운로드
+function downloadImage(imageUrl) {
+  const link = document.createElement("a");
+  link.href = imageUrl;
+  link.download = imageUrl.split("/").pop();
+  link.click();
+}
